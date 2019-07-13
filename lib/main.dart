f3712979b237086eb2b7f44dbd3e3da838cba3ce
Ishan Khanna <ishan1604@gmail.com>
2019-07-13 19:41:44 +0200

    Step 5: Click on FlutFlix, it'll suggest you to implement the build method

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
 class FlutFlix extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
   
 }
