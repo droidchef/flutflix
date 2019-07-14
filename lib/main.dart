a2478b4a15f6c03771eb2ad6732a481a7ecd0bb8
Ishan Khanna <ishan1604@gmail.com>
2019-07-14 23:09:17 +0200

    Step 30: Remove the card to show only images for now!

--- lib/main.dart
+++ lib/main.dart
@@ -21,27 +21,7 @@ class FlutFlix extends StatelessWidget {
                   return Padding(
                     padding: const EdgeInsets.symmetric(
                         horizontal: 16.0, vertical: 16.0),
-                    child: Card(
-                      child: ListTile(
-                        title: Text(
-                          '$index ' + movies[index % 4],
-                        ),
-                        leading: CircleAvatar(
-                          child: Image.asset('data-repo/img/bg1.jpg'),
-                        ),
-                        trailing: IconButton(
-                          icon: Icon(
-                            Icons.movie_creation,
-                            color: (index % 2 == 0
-                                ? Colors.deepPurple
-                                : Colors.purple),
-                          ),
-                          onPressed: () {
-                            print('Pressed Icon Number $index');
-                          },
-                        ),
-                      ),
-                    ),
+                    child: Image.asset('data-repo/img/bg1.jpg')
                   );
                 },
                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
