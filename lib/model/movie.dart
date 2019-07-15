d75573a4e7a3f391611cf3b79199837ccb904991
Ishan Khanna <ishan1604@gmail.com>
2019-07-15 03:55:22 -0700

    Step 36: Extract model into its own file

---
+++ lib/model/movie.dart
@@ -0,0 +1,41 @@
+
+class Movie {
+
+    final int id;
+
+    final String title;
+    
+    final String posterPath;
+
+    final String backdropPath;
+
+    final String overview;
+
+    final String releaseDate;
+
+    final double voteAverage;
+
+    Movie(this.id, this.title, this.posterPath, this.backdropPath, this.overview, this.releaseDate, this.voteAverage);
+
+    Movie.fromJson(Map<String, dynamic> json) 
+      : id = json['id'],
+        title = json['title'],
+        posterPath = json['poster_path'],
+        backdropPath = json['backdrop_path'],
+        overview = json['overview'],
+        releaseDate = json['release_date'],
+        voteAverage = json['vote_average'];
+        
+    
+    Map<String, dynamic> toJson() => 
+    {
+      'id' : id,
+      'title': title,
+      'poster_path': posterPath,
+      'backdrop_path': backdropPath,
+      'overview': overview,
+      'release_date': releaseDate,
+      'vote_average': voteAverage
+    };
+
+}
