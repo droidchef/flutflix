9db1a6b89b2b43679b232f82a0095d71b7808128
Ishan Khanna <ishan1604@gmail.com>
2019-07-15 04:03:26 -0700

    Step 42: Add the API Key and Baseurl

--- lib/repository/movies_repository.dart
+++ lib/repository/movies_repository.dart
@@ -5,7 +5,8 @@ import 'dart:convert';
 class MoviesRepository {
 
   Client httpClient = Client();
-  
+  final _apiKey = 'da0c0e40fa23a956aec2d5aa48032b32';
+  final _baseUrl = "http://api.themoviedb.org/3/movie";
 
 
   
