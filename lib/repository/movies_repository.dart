7ace354e9287fb22895d675d2ffd6f26ab5ab59a
Ishan Khanna <ishan1604@gmail.com>
2019-07-15 04:08:14 -0700

    Step 44: Create the Request URL using uri template

--- lib/repository/movies_repository.dart
+++ lib/repository/movies_repository.dart
@@ -2,12 +2,18 @@ import 'package:http/http.dart' show Client;
 import 'dart:async';
 import 'dart:convert';
 
+import 'package:uri/uri.dart';
+
 class MoviesRepository {
 
   Client httpClient = Client();
   final _apiKey = 'da0c0e40fa23a956aec2d5aa48032b32';
   final _baseUrl = "http://api.themoviedb.org/3/movie";
 
+  void nowPlaying() {
+    var template = UriTemplate("$_baseUrl/now_playing{?api_key}");
+    String requestUrl = template.expand({"api_key": _apiKey});
+  }
 
   
 }
