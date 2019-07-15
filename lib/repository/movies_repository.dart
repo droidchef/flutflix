13388f8879bf5efb3eb60cfe390b4de3632e8ab0
Ishan Khanna <ishan1604@gmail.com>
2019-07-15 06:12:36 -0700

    Step 52: Make the nowPlaying method return a Future<MoviesResult> so we can build our grid on the fly

--- lib/repository/movies_repository.dart
+++ lib/repository/movies_repository.dart
@@ -11,13 +11,14 @@ class MoviesRepository {
   final _apiKey = 'da0c0e40fa23a956aec2d5aa48032b32';
   final _baseUrl = "http://api.themoviedb.org/3/movie";
 
-  void nowPlaying() async {
+  Future<MoviesResult> nowPlaying() async {
     var template = UriTemplate("$_baseUrl/now_playing{?api_key}");
     String requestUrl = template.expand({"api_key": _apiKey});
     final response = await httpClient.get(requestUrl);
     if (response.statusCode == 200) {
       var mr = MoviesResult.fromJson(json.decode(response.body));
       print(mr.movies);
+      return mr;
     } else {
       throw Exception('We couldn\'t fetch movies because of the error code ${response.statusCode}');
     }
