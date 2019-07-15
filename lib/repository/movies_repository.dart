4f0f4154cdd1a601c63bd596f0d2cc4a9711039c
Ishan Khanna <ishan1604@gmail.com>
2019-07-15 04:16:36 -0700

    Step 46: get the response and parse it inline using dart convert\'s jsonDecode method

--- lib/repository/movies_repository.dart
+++ lib/repository/movies_repository.dart
@@ -10,10 +10,15 @@ class MoviesRepository {
   final _apiKey = 'da0c0e40fa23a956aec2d5aa48032b32';
   final _baseUrl = "http://api.themoviedb.org/3/movie";
 
-  void nowPlaying() {
+  void nowPlaying() async {
     var template = UriTemplate("$_baseUrl/now_playing{?api_key}");
     String requestUrl = template.expand({"api_key": _apiKey});
-    final response = httpClient.get(requestUrl);
+    final response = await httpClient.get(requestUrl);
+    if (response.statusCode == 200) {
+      print(jsonDecode(response.body));
+    } else {
+      throw Exception('We couldn\'t fetch movies because of the error code ${response.statusCode}');
+    }
   }
 
   
