d21869e31a7ed980dba5c67af52b554f869dfb8e
Ishan Khanna <ishan1604@gmail.com>
2019-07-15 06:11:33 -0700

    Step 50: Extract out the grid into its own widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'package:flutflix/model/movies_result.dart';
+import 'package:flutflix/repository/movies_repository.dart';
 import 'package:flutter/material.dart';
+
+import 'model/movie.dart';
+import 'dart:async';
 
 void main() => runApp(FlutFlix());
 
@@ -6,7 +11,6 @@ class FlutFlix extends StatelessWidget {
   var movies = ['Alpha', 'Beta', 'Gamma', 'Delta'];
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       title: 'FlutFlix',
       home: Scaffold(
@@ -16,23 +20,9 @@ class FlutFlix extends StatelessWidget {
         ),
         body: Center(
           child: Container(
-              color: Colors.green,
-              child: GridView.builder(
-                itemBuilder: (BuildContext context, int index) {
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 16.0, vertical: 16.0),
-                    child: Image.asset(
-                      'data-repo/img/bg1.jpg',
-                      fit: BoxFit.fill,
-                    ),
-                  );
-                },
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  childAspectRatio: 2 / 3,
-                ),
-              )),
+            color: Colors.green,
+            child: MoviesGrid(),
+          ),
         ),
       ),
     );
@@ -41,19 +31,30 @@ class FlutFlix extends StatelessWidget {
 
 class MoviesGrid extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return null;
-  }
-
+  State<StatefulWidget> createState() => _MovieGridState();
 }
 
 class _MovieGridState extends State<MoviesGrid> {
+  
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
+    return Container(
+      child: GridView.builder(
+        itemBuilder: (BuildContext context, int index) {
+          return Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+            child: Image.asset(
+              'data-repo/img/bg1.jpg',
+              fit: BoxFit.fill,
+            ),
+          );
+        },
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 2 / 3,
+        ),
+      ),
+    );
   }
-
 }
-
